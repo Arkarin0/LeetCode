45e11f11d781e92002f1c767732e5db3001cf957
Arkarin0 <snaut@hotmail.de>
2023-10-28 22:40:06 +0200

    solved problem 876 Middle of Linked List

---
+++ src/BeginnersGuide.Tests/P876_MiddleOfLinkedListTests.cs
@@ -0,0 +1,50 @@
+﻿using Xunit;
+using BeginnersGuide;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FluentAssertions;
+
+namespace BeginnersGuide.Tests
+{
+    public class P876_MiddleOfLinkedListTests
+    {
+        [Theory()]
+        [InlineData(5,3)]
+        [InlineData(6, 4)]
+        public void MiddleNodeTest(int count, int expectedValue)
+        {
+            //arrange
+            P876_MiddleOfLinkedList.ListNode priveous = null, startItem = null, expected = null;
+
+            for (int i = 1;  i <= count && i<=100; i++) 
+            { 
+                var node= new P876_MiddleOfLinkedList.ListNode();
+                node.val = i;
+
+                if(startItem is null)
+                {
+                    startItem = node;                    
+                }
+                else
+                {
+                    priveous.next = node;
+                }
+
+                priveous = node;
+
+                if(i== expectedValue) expected = node;
+            }
+
+            var dut= new P876_MiddleOfLinkedList();
+
+            //action
+            var actual = dut.MiddleNode(startItem);
+
+            //assert
+            actual.Should().Be(expected);
+        }
+    }
+}
---
+++ src/BeginnersGuide/P876_MiddleOfLinkedList.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BeginnersGuide
+{
+
+    // https://leetcode.com/problems/middle-of-the-linked-list/
+    public class P876_MiddleOfLinkedList
+    {
+
+        public class ListNode
+        {
+            public int val;
+            public ListNode next;
+            public ListNode(int val = 0, ListNode next = null)
+            {
+                this.val = val;
+                this.next = next;
+            }
+        }
+
+        public ListNode MiddleNode(ListNode head)
+        {
+            int count = 1;
+            ListNode node = head;
+            Dictionary<int, ListNode> dic= new Dictionary<int, ListNode>()
+            {
+                { 1, node },
+            };
+            
+            while (node.next != null)
+            {
+                node = node.next;
+                count++;
+
+                dic.Add(count, node );
+            }
+
+            if (count == 1) { return head; }
+            else if( count %2 != 0 ) { count--; }
+
+            var id = (count / 2) + 1;
+
+            return dic[id];            
+        }
+
+        public ListNode MiddleNodeSuperFast(ListNode head)
+        {
+            ListNode oneStepPointer = head;
+            ListNode twoStepPointer = head;
+
+            while (twoStepPointer.next != null)
+            {
+                oneStepPointer = oneStepPointer.next;
+                twoStepPointer = twoStepPointer.next.next;
+
+                if (twoStepPointer == null)
+                    break;
+            }
+
+            return oneStepPointer;
+        }
+
+    }
+}
