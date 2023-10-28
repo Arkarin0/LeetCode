a371f379b802c2dccf16b10ac600a6f713941628
Arkarin0 <snaut@hotmail.de>
2023-10-28 22:40:25 +0200

    solved Problem 383 Ransom Note

---
+++ src/BeginnersGuide.Tests/P383_RansomNoteTests.cs
@@ -0,0 +1,28 @@
+﻿using Xunit;
+using BeginnersGuide;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FluentAssertions;
+
+namespace BeginnersGuide.Tests
+{
+    public class P383_RansomNoteTests
+    {
+        [Theory]
+        [InlineData("a","b",false)]
+        [InlineData("aa", "ab", false)]
+        [InlineData("aa", "aab", true)]
+        [InlineData("baa", "aba", true)]
+        public void CanConstructTest(string ransomeNote,string magazine, bool expectedValue)
+        {
+            var dut = new P383_RansomNote();
+
+            var actual = dut.CanConstruct(ransomeNote, magazine);
+
+            actual.Should().Be(expectedValue);
+        }
+    }
+}
---
+++ src/BeginnersGuide/P383_RansomNote.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BeginnersGuide
+{
+    public class P383_RansomNote
+    {
+        public bool CanConstruct(string ransomNote, string magazine)
+        {
+            var dicMag= new Dictionary<char, int>();
+            var dicNote = new Dictionary<char, int>();
+
+            foreach(var r in ransomNote)
+            {
+                if(!dicNote.ContainsKey(r))
+                {
+                    dicNote.Add(r, 1);
+                }
+                else
+                    dicNote[r]++;
+            }
+
+            foreach (var r in magazine)
+            {
+                if (!dicMag.ContainsKey(r))
+                {
+                    dicMag.Add(r, 1);
+                }
+                else
+                    dicMag[r]++;
+            }
+
+            int val = 0;
+            foreach (var pair in dicNote)
+            {
+                bool isInMagazine = dicMag.TryGetValue(pair.Key, out val);
+                if (!isInMagazine) return false;
+
+                bool notEnoughtLetters = val < pair.Value;
+                if (notEnoughtLetters) return false;
+            }
+
+            return true;
+        }
+    }
+}
